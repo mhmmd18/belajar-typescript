8e82e8d52c31e5ad7b99a4eac441ae934e2ac734
mhmmd18 <mamad180200@gmail.com>
2024-05-18 22:02:21 +0700

    function interface

--- dist/tests/interface.test.js
+++ dist/tests/interface.test.js
@@ -1,4 +1,5 @@
 describe('interface', function () {
+    // interface
     it('should interface', function () {
         const seller = {
             id: 1,
@@ -9,6 +10,13 @@ describe('interface', function () {
         // phone tidak bisa diubah isi nya, karena readonly properties
         // seller.phone = "081234567891"
         console.info(seller);
+    });
+    // function interface
+    it('should function interface', function () {
+        const add = (a, b) => {
+            return a + b;
+        };
+        console.info(add(1, 2));
     });
 });
 export {};
--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -1,6 +1,7 @@
 import { Seller } from "../src/seller"
 
 describe('interface', function () {
+    // interface
     it('should interface', function () {
         const seller: Seller = {
             id: 1,
@@ -11,5 +12,15 @@ describe('interface', function () {
         // phone tidak bisa diubah isi nya, karena readonly properties
         // seller.phone = "081234567891"
         console.info(seller);
+    })
+    // function interface
+    it('should function interface', function () {
+        interface addFunction {
+            (a: number, b: number): number
+        }
+        const add: addFunction = (a: number, b: number) => {
+            return a + b
+        }
+        console.info(add(1, 2));
     })
 })
