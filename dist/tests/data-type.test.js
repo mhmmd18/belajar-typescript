a888764dfb96c719de9b5b065ddfd41a57fd7b5d
mhmmd18 <mamad180200@gmail.com>
2024-05-18 14:53:46 +0700

    type data premitif

---
+++ dist/tests/data-type.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe('Data Type', function () {
+    it('should must declare', function () {
+        const name = "Mamad";
+        const belance = 1000;
+        const isVip = true;
+        console.info(name);
+        console.info(belance);
+        console.info(isVip);
+    });
+});
---
+++ tests/data-type.test.ts
@@ -0,0 +1,11 @@
+describe('Data Type', function () {
+    it('should must declare', function () {
+        const name: String = "Mamad";
+        const belance: Number = 1000;
+        const isVip: Boolean = true;
+
+        console.info(name);
+        console.info(belance);
+        console.info(isVip);
+    })
+})
