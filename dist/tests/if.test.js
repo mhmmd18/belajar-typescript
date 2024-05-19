74136bc68f570d9280ea232bf297e63d78b6ee51
mhmmd18 <mamad180200@gmail.com>
2024-05-19 16:00:56 +0700

    if statement

---
+++ dist/tests/if.test.js
@@ -0,0 +1,33 @@
+"use strict";
+describe('if statement', function () {
+    it("should if", function () {
+        const value = 85;
+        if (value >= 80) {
+            console.info("Congrats");
+        }
+        else if (value >= 60) {
+            console.info("Good");
+        }
+        else {
+            console.info("Bad");
+        }
+    });
+    // ternary operator
+    it('should ternary operator', function () {
+        const value = 80;
+        const hasil = value >= 80 ? "Good" : "Bad";
+        console.info(hasil);
+    });
+    // swith case
+    it('should switch case', function () {
+        function sayHello(name) {
+            switch (name) {
+                case "Muhammad":
+                    return "Hello Muhammad";
+                default:
+                    return "Hello";
+            }
+        }
+        console.info(sayHello("Muhammad"));
+    });
+});
